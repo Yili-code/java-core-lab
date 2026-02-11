0f562cc6284e3a76526d9422137eefb0828814e9
Yili <Yili.code@gmail.com>
2026-02-11 16:37:45 +0800

    Random

---
+++ practice/p001.java
@@ -0,0 +1,29 @@
+package practice;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class p001 {
+  public static void main(String[] args) {
+    Random r = new Random();
+    int target = r.nextInt(1, 100) + 1;
+    System.out.printf("%d%n", target);
+
+    try (Scanner sc = new Scanner(System.in)) {
+      int count = 0;
+      while (count < 10) {
+        int guessNumber = sc.nextInt();
+
+        if (guessNumber == target) {
+          System.out.println("You got it.");
+          break;
+        } else if (guessNumber < target) {
+          System.out.printf("The answer is bigger than %d%n", guessNumber);
+        } else if (guessNumber > target) {
+          System.out.printf("The answer is smaller than %d%n", guessNumber);
+        }
+        count++;
+      }
+    }
+  }
+}
